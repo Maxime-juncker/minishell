036a79e9cb9638f2c43cdf88cac5cee896ee9fe7
Alexis Bidolet--Foray <abidolet@z3r9p2.42lyon.fr>
2025-02-03 12:49:14 +0100

    redirection > >> work

--- srcs/init.c
+++ srcs/init.c
@@ -76,44 +76,30 @@ char	*expand_env_var(const char *str, char **env, int last_cmd)
 
 static int	handle_redirection(t_command *cmd, char *cmd_str, int is_last)
 {
-	char	*input_file;
 	char	*output_file;
+	char	*redirection_pos;
 
-	input_file = ft_strchr(cmd_str, '<');
-	if (input_file)
+	redirection_pos = ft_strchr(cmd_str, '>');
+	if (redirection_pos)
 	{
-		*input_file = '\0';
-		input_file++;
-		while (*input_file == ' ' || *input_file == '\t')
-			input_file++;
-		cmd->fd_in = open(input_file, O_RDONLY);
-		if (cmd->fd_in == -1)
+		if (*(redirection_pos + 1) == '>')
 		{
-			perror("Error opening input file");
-			return (0);
-		}
-		cmd->n_args -= 2;
-	}
-	output_file = ft_strchr(cmd_str, '>');
-	if (output_file)
-	{
-		output_file = ft_strdup(output_file);
-		*output_file = '\0';
-		output_file++;
-		while (*output_file == ' ' || *output_file == '\t')
-			output_file++;
-		if (ft_strnstr(cmd_str, ">>", ft_strlen(cmd_str)))
+			output_file = ft_strdup(redirection_pos + 2);
+			*redirection_pos = '\0';
 			cmd->fd_out = open(output_file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-		else
-			cmd->fd_out = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (cmd->fd_out == -1)
-		{
-			perror("Error opening output file");
-			return (0);
 		}
+		else
+		{
+			output_file = ft_strdup(redirection_pos + 1);
+			*redirection_pos = '\0';
+			cmd->fd_out = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		}
+		free(output_file);
+		if (cmd->fd_out == -1)
+			return (0);
 		cmd->n_args -= 2;
 	}
-	if (is_last && !ft_strchr(cmd_str, '>'))
+	else if (is_last)
 		cmd->fd_out = 1;
 	return (1);
 }
@@ -157,9 +143,7 @@ static int	init_cmd(t_command *cmd, char *cmd_str, char **env, int is_last, int 
 int	init_table(char *line, char **env, t_command_table *table, int last_cmd)
 {
 	char				**cmd_strs;
-	int					pipe_fd[2];
 	size_t				i;
-	t_command			*temp;
 
 	cmd_strs = ft_split(line, '|');
 	if (!cmd_strs)
@@ -167,7 +151,6 @@ int	init_table(char *line, char **env, t_command_table *table, int last_cmd)
 	table->n_commands = 0;
 	while (cmd_strs[table->n_commands])
 		table->n_commands++;
-	temp = table->commands;
 	table->commands = malloc(sizeof(t_command) * table->n_commands);
 	if (!table->commands)
 		return (127);
