fdc6c0bd165fc37f1f680f337eb03211320f2468
Maxime Juncker <maxime.junck@gmail.com>
2025-02-06 14:16:20 +0100

    fix faulty tests
    fix: pipe_after_redir and cat < out tests were reporting ko instead of ok

--- better-libunit/tests/commands/06_pipe_after_redir.cpp
+++ better-libunit/tests/commands/06_pipe_after_redir.cpp
@@ -2,13 +2,18 @@
 
 int	pipe_after_redir( void )
 {
+	const char *exepted_file[]
+	{
+		"0",
+	};
 	t_command_table	table;
 	table.env = environ;
+	init_table((char *)"echo test | wc -c > 1 | wc -l", environ, &table, 0);
 
-	init_table((char *)"echo test | wc -c > log.txt | wc -l", environ, &table, 0);
 	Libunit::Redirect_log();
 	run_pipeline(&table);
-	if (Libunit::Check_output("1\n") == 0)
+
+	if (Libunit::CheckFile("log.txt", exepted_file, 1) == 0)
 		return (0);
 	else
 		return (1);
